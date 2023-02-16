51e3c03e6aa1e508982cd5cafe4027d7a12ec7dd
Briana1984 <brian.a.03@hotmail.com>
2023-02-16 15:48:56 -0800

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include<stdio.h>
+   /**
+    *main - a C program that prints exactly "programming is like buildding
+    *Return: 0 (Success)
+    */
+int main(void)
+{
+  puts("\"programming is like buiding a multilingual puzzle");
+  return (0);
+}
