21d203a91a24120dcff55f9d1f632e3de2807842
Briana1984 <brian.a.03@hotmail.com>
2023-02-28 08:16:51 -0800

    add new search feature

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - a function that returns the length of a string.
+ * @s: void value add
+ * return 0
+ */
+int _strlen(char *s)
+{
+char i;
+i = strlen(s);
+return (i);
+}
